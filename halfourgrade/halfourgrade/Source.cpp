a6ebd6d1165dc067193740132ae8b426f395e72b
Gideon Jones <gideon.w.jones@gmail.com>
2020-06-11 10:46:53 -0600

    Init Commit

---
+++ halfourgrade/halfourgrade/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+//Sounds
+#include <Windows.h>
+#include <MMSystem.h>
+//Randoms & Math
+#include <random>
+#include <cstdlib>
+#include <ctime>
+#include <math.h>
+//Dynamic Arrays
+#include <vector>
+// Sleep Function
+#include <time.h>
+
+using namespace std;
+
+int main() {
+	cout << "Get Out." << endl;
+
+	system("PAUSE");
+	return 0;
+}
